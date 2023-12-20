922000199b9e7f07a799ce640e8b06fedf1b0b11
John Apt <jwa824@hotmail.com>
2023-12-20 08:26:52 -0600

    Example binding

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,11 +15,38 @@ namespace WPF
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow : Window, INotifyPropertyChanged
 	{
+		// INotifyPropertyChanged implementation
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		// BindingTestCounter property
+		private int _bindingTestCounter = 0;
+		
+		public int BindingTestCounter
+		{
+			get { return _bindingTestCounter; }
+			set
+			{
+				_bindingTestCounter = value;
+				OnPropertyChanged(nameof(BindingTestCounter));
+			}
+		}
+
+		// BindingTestCounterIncrement button click
+		private void BindingTestCounterIncrement_Click(object sender, RoutedEventArgs e)
+		{
+			BindingTestCounter++;
+		}
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			this.DataContext = this;
 		}
 	}
 }
