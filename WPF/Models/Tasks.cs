fdc7f25a1f49628990e79ec4dd6f6d9a9a3abc8a
Benji Altman <benjidaltman@gmail.com>
2023-12-20 09:00:26 -0600

    user control addd

---
+++ WPF/Models/Tasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.Models
+{
+    class BasicTask
+    {
+		public DateTime Start;
+		public TimeSpan Duration;
+		public required string Text;
+		public required string Name;
+		public DateTime End => Start + Duration;
+    }
+}
